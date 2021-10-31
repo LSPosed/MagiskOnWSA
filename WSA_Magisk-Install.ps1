ca307bfed817b1147498626b58c3bbc28402ed11
Lulutima <75037983+Lulutima@users.noreply.github.com>
2021-10-31 02:03:13 +0200

    Add WSA_Magisk-Install.ps1
    A small script to place at the root of the project and takes care of everything

---
+++ WSA_Magisk-Install.ps1
@@ -0,0 +1,16 @@
+﻿try
+{
+    $User = [Security.Principal.WindowsIdentity]::GetCurrent()
+    $Role = (New-Object Security.Principal.WindowsPrincipal $user).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
+    if(!$Role)
+    {
+        Throw "Run as an administrator !" 
+    }
+    import-module appx -usewindowspowershell
+    Remove-AppxPackage MicrosoftCorporationII.WindowsSubsystemForAndroid_1.7.32815.0_x64__8wekyb3d8bbwe -AllUsers
+    Add-AppxPackage -Register .\AppxManifest.xml
+}
+catch
+{
+    Write-Host $_.Exception.Message -ForegroundColor Red
+}
